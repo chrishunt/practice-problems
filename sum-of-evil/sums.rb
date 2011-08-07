6626177f4ab28749ac692488f0c794d55bf01e04
Chris Hunt <huntca@gmail.com>
2011-08-07 09:14:37 -0700

    Implemented recursive permutation printing practice problem, sums.rb

--- sum-of-evil/sums.rb
+++ sum-of-evil/sums.rb
@@ -9,3 +9,32 @@
 # 1+2-3
 # 12+3
 # 12-3
+
+def print_perms(string, chars, position=0, current_perm="")
+  string_array = string.split("")
+
+  # If we have an empty string, then print no permutations
+  if (string_array.size < 1)
+    puts ""
+    return
+  end
+
+  # If we're done with this perm, print it
+  if (position == (string_array.size - 1))
+    puts current_perm + string_array.last
+    return
+  end
+
+  # Print all permutations
+  chars.each do |char|
+    next_perm = current_perm + string_array[position] + char
+    print_perms(string, chars, position+1, next_perm)
+  end
+end
+
+# Special case of print_perms for this example
+def print_sums(string)
+  print_perms(string, ["","-","+"])
+end
+
+print_sums("123")
