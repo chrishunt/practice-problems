74d0edc994c36cf321b1ad0011e885c5a6df751a
Chris Hunt <huntca@gmail.com>
2011-08-06 21:26:19 -0700

    Initial commit

---
+++ sum-of-evil/sums.rb
@@ -0,0 +1,11 @@
+# Given a number, come up with all of the possible ways to insert '+' and '-'
+# in that number. For example given 123, possible answer would be:
+#
+# 1+23
+# 1+2+3
+# 1-23
+# 1-2+3
+# 1-2-3
+# 1+2-3
+# 12+3
+# 12-3
